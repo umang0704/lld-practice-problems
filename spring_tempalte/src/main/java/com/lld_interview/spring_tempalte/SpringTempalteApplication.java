15fc6e698b9da34ea6d231916d59b3beabcaa33e
Umang Yadav <umang.work82@gmail.com>
2025-04-28 23:03:11 +0530

    added spring template

---
+++ spring_tempalte/src/main/java/com/lld_interview/spring_tempalte/SpringTempalteApplication.java
@@ -0,0 +1,15 @@
+package com.lld_interview.spring_tempalte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.text.MessageFormat;
+
+@SpringBootApplication
+public class SpringTempalteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTempalteApplication.class, args);
+	}
+
+}
---
+++ spring_tempalte/src/test/java/com/lld_interview/spring_tempalte/SpringTempalteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lld_interview.spring_tempalte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTempalteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
